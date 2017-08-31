1e964631f487cd43e4b0ecb243a3075911a8f50f
singleShadow <793284979@qq.com>
2017-08-31 17:35:16 +0800

    template syntax change

--- js/workProject.js
+++ js/workProject.js
@@ -1,7 +1,56 @@
-Vue.component('nav-component', {
+/*Vue.component('navChild', {                   //子组件
 	template: `<a href="#">{{ todo.title }}</a>`,
 	props: ['todo']	
-})
+});
+Vue.component('nav-component', {               //父组件
+	template: `
+			<header>
+			<nav>
+				<navChild v-for="item in pagesTitle" v-bind:todo="item"></navChild>
+			</nav>
+		</header>
+		`,
+	components: {
+		'navChild': navChild
+	},
+	data: function(){
+  	return {
+			pagesTitle: [
+				{title: '主页'},
+				{title: '项目'},
+				{title: '爱好'},
+				{title: '个人'}
+			]
+		}
+  }
+});*/
+var navList=Vue.extend({   //定义一个子组件，组件内的li标签想v-for出来
+  template:'<a href="#">{{ todo.title }}</a>',
+  props:['todo']
+});
+
+var navigation=Vue.extend({  //定义父组件，在父组件内部注册子组件
+  template:`
+		<header>
+			<nav>
+				<nav-list v-for="item in pagesTitle" v-bind:todo="item"></nav-list>
+			</nav>
+		</header>
+		`,
+  components:{
+    'nav-list':navList
+  },
+  data: function(){
+  	return {
+			pagesTitle: [
+				{title: '主页'},
+				{title: '项目'},
+				{title: '爱好'},
+				{title: '个人'}
+			]
+		}
+  }
+});
 var vm = new Vue({
 	el: '#mainPage',
 	data: {
@@ -11,5 +60,9 @@ var vm = new Vue({
 			{title: '爱好'},
 			{title: '个人'}
 		]
+	},
+	components: {
+		'navigation': navigation
+		// 'nav-component': nav-component
 	}
 })
